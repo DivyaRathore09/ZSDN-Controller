cbcf45d7b033c20df467dd7634f8ce21cac7ccc3
Divya Rathore <rthrdivya09@gmail.com>
2020-04-27 13:34:34 +0530

    Modules

--- Project_Modules/Install_Docker_Script.sh
+++ Project_Modules/Install_Docker_Script.sh
@@ -2,14 +2,28 @@
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
+echo "Done1"
+
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+echo "Done2"
 
 sudo apt-get update
 
+echo "Done3"
+
 apt-cache policy docker-ce
+
+echo "Done4"
 
 sudo apt-get install -y docker-ce
 
+echo "Done5"
+
 sudo -i
 
+echo "Done6"
+
 docker run hello-world
+
+echo "Done7"
